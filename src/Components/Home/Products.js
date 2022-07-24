31e96df2dcc60883ed12d017ccff6e27c86c3b5d
sohag sagar <sagorsohag29@gmail.com>
2022-07-25 04:44:28 +0600

    use react query to make single query for filtering data more efficiently

--- src/Components/Home/Products.js
+++ src/Components/Home/Products.js
@@ -1,91 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import Loading from '../SharedComponents/Loading';
 import Product from './Product';
+import { useQuery } from 'react-query';
 
 const Products = () => {
-    const [products, setProducts] = useState([]);
+
+    const [category, setCategory] = useState('')
     const [activeBtn, setActiveBtn] = useState('allProducts');
     const [searchedText, setSearchedText] = useState('');
-    const [loadingStatus,setLoadingStatus]=useState(false);
 
+    const { data: products, isLoading } = useQuery(['products', category], () => fetch(`http://localhost:5000/products/${category}`).then(res => res.json()));
 
-    useEffect(() => {
-        setLoadingStatus
-        fetch('http://localhost:5000/products')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, [])
-
-    if (products.length < 0) {
-        return <Loading />
-    }
-
-
-
+  
     const allProducts = () => {
-        fetch(`http://localhost:5000/products`)
-            .then(res => res.json())
-            .then(data => {
-                const allProducts = data;
-                if (!allProducts) {
-                    return <Loading />
-                }
-                setProducts(allProducts);
-                setActiveBtn('allProducts');
-                document.getElementById('search-input').value = '';
-                setSearchedText('');
-
-            })
+        setCategory('')
+        setActiveBtn('allProducts');
+        document.getElementById('search-input').value = '';
+        setSearchedText('');
     }
 
 
     const asusProducts = () => {
-        const category = 'asus';
-        fetch(`http://localhost:5000/products/${category}`)
-            .then(res => res.json())
-            .then(data => {
-                const allProducts = data;
-                if (allProducts.length < 0) {
-                    return <Loading />
-                }
-                setProducts(allProducts);
-                setActiveBtn('asusProducts');
-                document.getElementById('search-input').value = '';
-                setSearchedText('');
-            })
+        setCategory('asus');
+        setActiveBtn('asusProducts');
+        document.getElementById('search-input').value = '';
+        setSearchedText('');
     }
 
     const hpProducts = () => {
-        const category = 'hp';
-        fetch(`http://localhost:5000/products/${category}`)
-            .then(res => res.json())
-            .then(data => {
-                const allProjects = data;
-                if (allProjects.length < 0) {
-                    return <Loading />
-                }
-                setProducts(allProjects);
-                setActiveBtn('hpProducts');
-                document.getElementById('search-input').value = '';
-                setSearchedText('');
-            })
+        setCategory('hp');
+        setActiveBtn('hpProducts');
+        document.getElementById('search-input').value = '';
+        setSearchedText('');
     }
 
     const lenovoProducts = () => {
-        const type = 'lenovo';
-        fetch(`http://localhost:5000/products/${type}`)
-            .then(res => res.json())
-            .then(data => {
-                const allProducts = data;
-                if (allProducts.length < 0) {
-                    return <Loading />
-                }
-                setProducts(allProducts);
-                setActiveBtn('lenovoProducts');
-                document.getElementById('search-input').value = '';
-                setSearchedText('');
-            })
+        setCategory('lenovo');
+        setActiveBtn('lenovoProducts');
+        document.getElementById('search-input').value = '';
+        setSearchedText('');
+
+
     }
+
+
     return (
         <div className='lg:px-12 lg: py-12 min-h-screen'>
 
@@ -98,7 +56,9 @@ const Products = () => {
                     <button onClick={lenovoProducts} className={`btn btn-sm rounded-full w-28 ${activeBtn === 'lenovoProducts' && "btn-primary"}`}>Lenovo</button>
                 </div>
             </div>
-
+            {
+                isLoading && <Loading />
+            }
             <div className='grid lg:grid-cols-4 sm:grid-cols-1 justify-center items-center'>
                 {
                     products?.filter((val) => {
